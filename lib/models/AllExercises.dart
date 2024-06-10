60b391c7c7ba7498acde7430bc633b6bd95464b5
jmg_ant_only <69183660+wkdgnl123@users.noreply.github.com>
2024-06-11 07:29:22 +0900

    0611_정민규

---
+++ lib/models/AllExercises.dart
@@ -0,0 +1,44 @@
+import 'Exercise.dart';
+
+List<Exercise> allExercises = [
+  //너무 지저분- 나중에 해결 예정 - 안될듯;;;
+
+  Exercise(name: "Dead Lift", muscleGroup: "Leg", equipment: "Barbell"),
+  Exercise(name: "Squat", muscleGroup: "Leg", equipment: "Barbell"),
+  Exercise(name: "Bench Press", muscleGroup: "Chest", equipment: "Barbell"),
+  Exercise(name: "Pull Up", muscleGroup: "Back", equipment: "Body Weight"),
+  Exercise(name: "Leg Press", muscleGroup: "Leg", equipment: "Machine"),
+  Exercise(
+      name: "Military Press", muscleGroup: "Shoulder", equipment: "Barbell"),
+  Exercise(name: "Bicep Curl", muscleGroup: "Arm", equipment: "Dumbbell"),
+  Exercise(name: "Tricep Dip", muscleGroup: "Arm", equipment: "Body Weight"),
+  Exercise(
+      name: "Lateral Raise", muscleGroup: "Shoulder", equipment: "Dumbbell"),
+  Exercise(name: "Leg Curl", muscleGroup: "Leg", equipment: "Machine"),
+  Exercise(name: "Chest Fly", muscleGroup: "Chest", equipment: "Machine"),
+  Exercise(name: "Rowing", muscleGroup: "Back", equipment: "Machine"),
+  Exercise(name: "Lat Pulldown", muscleGroup: "Back", equipment: "Cable"),
+  Exercise(name: "Front Squat", muscleGroup: "Leg", equipment: "Barbell"),
+  Exercise(name: "Sumo Deadlift", muscleGroup: "Leg", equipment: "Barbell"),
+  Exercise(name: "Calf Raise", muscleGroup: "Leg", equipment: "Machine"),
+  Exercise(
+      name: "Incline Bench Press", muscleGroup: "Chest", equipment: "Barbell"),
+  Exercise(
+      name: "Decline Bench Press", muscleGroup: "Chest", equipment: "Barbell"),
+  Exercise(
+      name: "Hyperextension", muscleGroup: "Back", equipment: "Body Weight"),
+  Exercise(name: "Crunch", muscleGroup: "Abs", equipment: "Body Weight"),
+  Exercise(
+      name: "Russian Twist", muscleGroup: "Abs", equipment: "Medicine Ball"),
+  Exercise(name: "Plank", muscleGroup: "Core", equipment: "Body Weight"),
+  Exercise(
+      name: "Hanging Leg Raise", muscleGroup: "Abs", equipment: "Body Weight"),
+  Exercise(name: "Box Jump", muscleGroup: "Leg", equipment: "Box"),
+  Exercise(name: "Burpee", muscleGroup: "Full Body", equipment: "Body Weight"),
+  Exercise(name: "T-Bar Row", muscleGroup: "Back", equipment: "Barbell"),
+  Exercise(name: "Seated Cable Row", muscleGroup: "Back", equipment: "Cable"),
+  Exercise(name: "Face Pull", muscleGroup: "Shoulder", equipment: "Cable"),
+  Exercise(name: "Hammer Curl", muscleGroup: "Arm", equipment: "Dumbbell"),
+  Exercise(name: "Skull Crusher", muscleGroup: "Arm", equipment: "Barbell")
+  // 다른 운동들 추가 노가다해야함;;;
+];
